2817b79799ada04f3f24cfef00eea63de0f99392
Digitalflake Mahesh K <digitalflakemaheshk@Digitalflakes-Mac-mini.local>
2024-01-22 16:27:13 +0530

    Add first commit

---
+++ DigitalflakePrj/DigitalflakePrj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DigitalflakePrj
+//
+//  Created by Digitalflake Mahesh K on 22/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
